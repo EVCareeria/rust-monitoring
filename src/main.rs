ad390b2348ee61ca14b2af78f73fbff414049756
evcareeria <erik.vesa.contact@gmail.com>
2024-09-28 16:44:51 +0300

    [Made some changes and added cpu data]

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,16 @@ use sysinfo::{
 
 fn main() {
     println!("Hello, world!");
-    system_info();
+    let mut sys = System::new_all();
+    memory_info(&mut sys);
+    system_meta_data();
+    cpu_info(&mut sys);
 }
 
-fn system_info() {
+fn memory_info(sys: &mut System) {
     
     // Please note that we use "new_all" to ensure that all lists of
     // CPUs and processes are filled!
-    let mut sys = System::new_all();
     
     // First we update all information of our `System` struct.
     sys.refresh_all();
@@ -19,20 +21,34 @@ fn system_info() {
     println!("=> system:");
     // RAM and swap information:
     let mut total_memory = sys.total_memory().to_string();
-    let (first, second) = total_memory.split_at(2);
-    let total_memory_after_comma: String = second.chars().into_iter().take(3).collect();
-    println!("total memory: {0},{1} gigabytes", first, total_memory_after_comma);
+    let memory_parts = total_memory.split_at(2);
+    let total_memory_after_comma: String = memory_parts.1.chars().into_iter().take(3).collect();
+    println!("total memory: {0},{1} gigabytes", memory_parts.0, total_memory_after_comma);
     println!("used memory : {} gigabytes", sys.used_memory());
     println!("total swap  : {} gigabytes", sys.total_swap());
     println!("used swap   : {} gigabytes", sys.used_swap());
+        
+}
+
+fn cpu_info(sys: &mut System) {
+    // Wait till our cpu data is fetched
+    std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+    // Update cpu data
+    sys.refresh_cpu_usage();
+
+    let cpus = sys.cpus();
+    println!("NB CPUs: {}", cpus.len());
     
-    // Display system information:
+    for core in cpus {
+        println!("Core {0} {1} % in use", core.name(), core.cpu_usage().round());
+    }
+
+}
+
+fn system_meta_data() {
+ // Display system information:
     println!("System name:             {:?}", System::name());
     println!("System kernel version:   {:?}", System::kernel_version());
     println!("System OS version:       {:?}", System::os_version());
     println!("System host name:        {:?}", System::host_name());
-    
-    // Number of CPUs:
-    println!("NB CPUs: {}", sys.cpus().len());
-    
 }
